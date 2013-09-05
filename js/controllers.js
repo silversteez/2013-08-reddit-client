a71cd46abb0d321dca1003a87a8dc8719f5459e8
matt silverstein <welcomematt@gmail.com>
2013-09-04 17:10:03 -0700

    pulling down and displaying articles, voting up/down works

--- js/controllers.js
+++ js/controllers.js
@@ -1,1 +1,28 @@
-angular.module('myApp.controllers', []);
+angular.module('myApp.controllers', [])
+.controller('MainController', [ '$scope', '$http', function($scope, $http) {
+
+  $http({
+    method: 'JSONP',
+    url: 'http://api.reddit.com/new?jsonp=JSON_CALLBACK'
+  })
+  .success(function(data, status, headers, config) {
+    $scope.articles = data.data.children;
+  })
+  .error(function(data, status, headers, config) {
+    console.log('error ', status);
+  });
+
+}])
+.controller('ArticleController', ['$scope', function($scope) {
+
+  $scope.votes = 0;
+
+  $scope.voteUp = function() {
+    $scope.votes++;
+  };
+
+  $scope.voteDown = function() {
+    $scope.votes--;
+  };
+
+}]);
--- js/directives.js
+++ js/directives.js
@@ -1,1 +1,14 @@
-angular.module('myApp.directives', []);
+angular.module('myApp.directives', [])
+.directive('mySweetDirective', function() {
+  return {
+    restrict: 'A',
+    // require: ['^ngModel'],
+    // scope: {
+    //   ngModel: '=',
+    // },
+    templateUrl: 'templates/home.html',
+    link: function(scope, ele, attr) {
+      // scope.votes = 5;
+    }
+  };
+});
